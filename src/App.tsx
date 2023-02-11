afbcd9a34ba630f2a3736cab34d527e1697f2071
vijaytembugade <vijaytembugade21@gmail.com>
2023-02-11 20:51:50 +0530

    linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import "./styles.css";
+import Counter from './Components/Counter';
+import './styles.css';
 const App = () => {
-  return <h2>App and website {process.env.environment}</h2>;
+  return (
+    <h2>
+      Environment: {process.env.environment}
+      <Counter />
+    </h2>
+  );
 };
 
 export default App;
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,10 +1,17 @@
 import webpack from "webpack";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+
 export const config = {
   mode: "development",
   devtool: "cheap-module-source-map",
+  devServer: {
+    hot: true,
+    open: true,
+  },
   plugins: [
     new webpack.DefinePlugin({
       "process.env.environment": JSON.stringify("development"),
     }),
+    new ReactRefreshWebpackPlugin(),
   ],
 };
